b0c3211d787d53fbc5eda76cdffe5c26e6952e13
Henman <nanahenman@yahoo.com>
2023-12-22 15:56:17 +0000

    Organize Project Folder structure

---
+++ src/app/shared/lostitems.js
@@ -0,0 +1,23 @@
+import wallet from '../assets/Images/wallet.jpg';
+import laptop from '../assets/Images/laptop.jpg';
+import watch from '../assets/Images/watch.jpg';
+import waterbottle from '../assets/Images/waterbottle.jpg';
+import umbrella from '../assets/Images/umbrella.jpg';
+import notebook from '../assets/Images/notebooks.jpg';
+import headphone from '../assets/Images/headphone.jpg';
+import phone from '../assets/Images/phone.jpg';
+import glasses from '../assets/Images/glasses.jpg';
+import keys from '../assets/Images/keys.jpg';
+
+const lostItems = [
+    { id: 0, name: "Key", image: keys, description: "Lost house key" },
+    { id: 1, name: "Wallet", image: wallet, description: "Brown leather wallet" },
+    { id: 2, name: "Phone", image: phone, description: "Black smartphone" },
+    { id: 3, name: "Glasses", image: glasses, description: "Reading glasses" },
+    { id: 4, name: "Watch", image: watch, description: "Silver wristwatch" },
+    { id: 5, name: "Umbrella", image: umbrella, description: "Blue compact umbrella" },
+    { id: 6, name: "Laptop", image: laptop, description: "15-inch laptop" },
+    { id: 7, name: "Headphones", image: headphone, description: "Wireless headphones" },
+    { id: 8, name: "Water Bottle", image: waterbottle, description: "Reusable water bottle" },
+    { id: 9, name: "Notebook", image: notebook, description: "Small notebook" },
+];
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './style/index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
